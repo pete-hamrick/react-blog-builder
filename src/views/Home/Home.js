72274be9ad99cb448cf3cf2103d44d938b55798a
Pete Hamrick <pete.hamrick@gmail.com>
2021-11-29 15:05:53 -0800

    added all state hooks

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,11 +10,14 @@ export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('')
   const [subtitle, setSubtitle] = useState('')
+  const [font, setFont] = useState('')
+  const [align, setAlign] = useState('')
+  const [text, setText] = useState('')
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle} />
+      <Preview title={title} subtitle={subtitle} font={font} align={align} text={text} />
       <Editor title={title} setTitle={setTitle} subtitle={subtitle} setSubtitle={setSubtitle} />
     </main>
   )
